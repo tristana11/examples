fa1ffc8419d9e5fc95e06630c2e90f89cdad1a9d
Nataliia Levak <nataliialevak@gmail.com>
2023-01-04 22:03:49 +0300

    feat:  fix

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int x = 23;
+        System.out.printf("Hello world! %d", x);
     }
 }
